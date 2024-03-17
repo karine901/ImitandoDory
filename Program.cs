e1e9188f1c7558f9b34295e62859ec0e602d0307
Meu karine <meu-km123silva2339@meu-karine901.com>
2024-03-17 14:14:45 -0300

    exercicio realizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("tradutor de baleês \n");
+Console.Write("digite o texto a ser traduzido: ");
+
+string texto = Console.ReadLine()!;
+
+Console.WriteLine("\nA dory diria ...\n");
+
+Console.WriteLine(texto
+.Replace("a" , "aaa")
+.Replace("i" , "iii")
+.Replace("u" , "uuu")
+.Replace("o" , "ooo")
+.Replace("a" , "aaa")
+.Replace("i" , "iii")
+.Replace("u" , "uuu")
+.Replace("o" , "ooo")
+);
+ Console.WriteLine();
+
